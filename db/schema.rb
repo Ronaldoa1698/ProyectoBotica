014d4037d472566fc0cdc0a70863c1718e731e5d
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2022-10-04 17:52:43 -0500

    add category, product and supplier tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_223432) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_224905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_223432) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.boolean "has_stock"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -29,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_223432) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
 end
