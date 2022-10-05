d6c64d2fd2cc2ca421ff9cd0bd8dd5bcb0d57a4a
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2022-10-04 20:17:03 -0500

    subiendo a nueva repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+      namespace :admin do
+      resources :suppliers
+      resources :products
+      resources :categories
+
+      root to: "suppliers#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
