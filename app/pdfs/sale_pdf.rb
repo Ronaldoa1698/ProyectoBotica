0620b9a39245f4db99be361df7cf69704a06bfbc
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2022-12-20 17:56:38 -0500

    corrections

--- app/pdfs/sale_pdf.rb
+++ app/pdfs/sale_pdf.rb
@@ -4,15 +4,12 @@ class SalePdf < Prawn::Document
         super(top_margin: 70)
         @sale = sale
         prueba
-        line_items
+        
     end
     def prueba
         text "hola", size: 20, style: :bold
     end
 
-    def line_items
-        move_down 20
-        table [[1,2], [3,4]]
-    end
+
 
 end
